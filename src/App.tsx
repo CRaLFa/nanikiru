3d95e4456aa0b52ad4706fc9216806d45c76a060
CRaLFa <minami.smd@gmail.com>
2024-09-12 12:01:50 +0900

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import { useState } from "react";
+import { PointerEvent, useState } from "react";
 import "./App.css";
+
+const HANDS_COUNT = 14;
 
 const initTiles = () => {
   const tiles = [...Array(136).keys()].map((n) => n % 34);
-  shufTiles(tiles);
-  return tiles;
-};
-
-const shufTiles = (tiles: number[]) => {
+  // shuffle tiles
   for (let i = tiles.length - 1; i >= 0; i--) {
     const r = Math.floor(Math.random() * (i + 1));
     [tiles[i], tiles[r]] = [tiles[r], tiles[i]];
   }
+  return tiles;
 };
 
 const sortTiles = (tiles: number[]) => {
@@ -23,23 +22,62 @@ const sortTiles = (tiles: number[]) => {
 
 function App() {
   let tiles = initTiles();
+
   const [hands, setHands] = useState<number[]>([]);
+  const [selected, setSelected] = useState(-1);
+  const [drawn, setDrawn] = useState(false);
 
   const refreshHands = () => {
+    setDrawn(false);
+    setSelected(-1);
     tiles = initTiles();
-    setHands(sortTiles(tiles.slice(0, 14)));
+    setHands(sortTiles(tiles.splice(0, HANDS_COUNT)));
+  };
+
+  const handClicked = (e: PointerEvent<HTMLImageElement>) => {
+    // console.log(e);
+    if (!(e.target instanceof HTMLImageElement)) {
+      return;
+    }
+    const idx = parseInt(e.target.id.replace("hand_", ""), 10);
+    if (idx === selected) {
+      if (tiles.length < 1) {
+        window.alert("山に牌がありません");
+        refreshHands();
+        return;
+      }
+      setSelected(-1);
+      setHands(() => {
+        let arr = [...hands];
+        arr.splice(idx, 1);
+        arr = sortTiles(arr);
+        arr.push(tiles.shift()!);
+        return arr;
+      });
+      setDrawn(true);
+    } else {
+      setSelected(idx);
+    }
   };
 
   return (
     <>
       <h1>何切る?</h1>
-      <div className="hands">
+      <div id="hands">
         {hands.map((hand, i) => (
-          <img
+          <div
             key={i}
-            src={`/png/U+${(0x1F000 + hand).toString(16).toUpperCase()}.png`}
-            className="hand"
-          />
+            className={`hand ${i === selected ? "selected" : ""} ${
+              drawn && i === hands.length - 1 ? "draw" : ""
+            }`}
+          >
+            <img
+              src={`/png/U+${(0x1F000 + hand).toString(16).toUpperCase()}.png`}
+              id={`hand_${i}`}
+              className="hand-img"
+              onClick={handClicked}
+            />
+          </div>
         ))}
       </div>
       <div className="buttons">
